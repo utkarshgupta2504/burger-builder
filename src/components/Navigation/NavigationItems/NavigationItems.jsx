f2e63802267eb88665bc01ef74c06ead224f823a
utkarshgupta2504 <utkarshgupta2504@gmail.com>
2020-11-11 02:01:01 +0530

    Added navigation tools (Sidebar, toolbar)

---
+++ src/components/Navigation/NavigationItems/NavigationItems.jsx
@@ -0,0 +1,14 @@
+import classes from "./NavigationItems.module.css";
+import React from "react";
+import NavigationItem from "./NavigationItem/NavigationItem";
+
+const NavigationItems = () => (
+	<ul className={classes.NavigationItems}>
+		<NavigationItem link="/" active>
+			Burger Builder
+		</NavigationItem>
+		<NavigationItem link="/">Checkout</NavigationItem>
+	</ul>
+);
+
+export default NavigationItems;
--- src/components/Navigation/SideDrawer/SideDrawer.jsx
+++ src/components/Navigation/SideDrawer/SideDrawer.jsx
@@ -1,18 +1,17 @@
+import classes from "./SideDrawer.module.css";
 import React from "react";
 import Logo from "../../Logo/Logo";
 import NavigationItems from "../NavigationItems/NavigationItems";
-import classes from "./Toolbar.module.css";
 
-const Toolbar = (props) => (
-	<header className={classes.Toolbar}>
-		<div>MENU</div>
-		<Logo />
-		<div>
+const SideDrawer = (props) => {
+	return (
+		<div className={classes.SideDrawer}>
+			<Logo />
 			<nav>
 				<NavigationItems></NavigationItems>
 			</nav>
 		</div>
-	</header>
-);
+	);
+};
 
-export default Toolbar;
+export default SideDrawer;
