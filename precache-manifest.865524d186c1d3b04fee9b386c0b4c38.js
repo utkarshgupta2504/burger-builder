f84b4cf97cb14430eb2a32288743cefdd75396ca
utkarshgupta2504 <utkarshgupta2504@gmail.com>
2020-11-11 04:37:34 +0530

    Updates

---
+++ precache-manifest.865524d186c1d3b04fee9b386c0b4c38.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79dc31c31c435ee56c6d3f3f1f938cfe",
+    "url": "/burger-builder/index.html"
+  },
+  {
+    "revision": "2c3f25d0c09740bc8555",
+    "url": "/burger-builder/static/css/main.367eb2c4.chunk.css"
+  },
+  {
+    "revision": "e28c76583fb687f679c4",
+    "url": "/burger-builder/static/js/2.d3c9c3fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/burger-builder/static/js/2.d3c9c3fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c3f25d0c09740bc8555",
+    "url": "/burger-builder/static/js/main.a9e514fa.chunk.js"
+  },
+  {
+    "revision": "3743a9caf0853cc6f96a",
+    "url": "/burger-builder/static/js/runtime-main.f3cd13ce.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger-builder/static/media/burger-logo.b8503d26.png"
+  }
+]);
